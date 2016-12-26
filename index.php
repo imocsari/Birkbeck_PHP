585f278e515ef04d68b7de75f16cb5d0cbf7ca34
Imre Mocsari <imremocsari1@yahoo.co.uk>
2016-12-26 14:47:58 +0000

    homepage

--- index.php
+++ index.php
@@ -5,11 +5,7 @@ if(!empty($_GET['status'])){
   echo '<div style="padding-top:200px; padding-bottom:800px; font-weight: bold; color: white; text-align:center; font-size:30px"><p>Successfully logged out!</p><br><p style="padding-top:50px;">Redirecting to homepage!</p></div>';
   header("Refresh: 3; url=index.php");
 }
-if (!isset($_SESSION['is_auth'])) {
-    echo '<div text-center><b>Login Required!</b></div>';
-    header("Location: ../login.php");
-    die;
-} else{
+if (isset($_SESSION['is_auth'])) {
     $r = '<div class="welcome text-center"><b>' . 'Welcome ' . $_SESSION['username'] . ' to Homepage! </b></div>';
     echo $r;
 }
