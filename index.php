739aab0c8b8631c253e49190ab52a0758f2acf3a
Imre Mocsari <imremocsari1@yahoo.co.uk>
2017-01-12 02:28:08 +0000

    div text

--- index.php
+++ index.php
@@ -11,13 +11,15 @@ if (isset($_SESSION['is_auth'])) {
     echo $name;
 }
 ?>
-
-<!-- <body>
-  <div class="maintext"> -->
+<!DOCTYPE html>
+<html>
+<body>
+  <div class="maintext">
     <h3>The Department of <b style="color:#fcab0e;">Computer Science</b> and Information Systems at Birkbeck</h3>
-      <!-- <div id="secondline"> -->
+      <div id="secondline">
         <p>One of the first computing departments established in the UK and we will be celebrating our 60th anniversary in 2017. We provide a stimulating teaching and research environment for both part-time and full-time students, and a friendly, inclusive space for learning, working and collaborating.</p>
-      <!-- </div> -->
+      </div>
   <?php include("layouts/footer.php"); ?>
-  <!-- </div>
-</body> -->
+  </div>
+</body>
+</html>
