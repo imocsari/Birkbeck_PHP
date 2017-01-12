edb474d4409ff95109bdddb1adfcfd8e7edccec5
Imre Mocsari <imremocsari1@yahoo.co.uk>
2017-01-12 13:27:23 +0000

    footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,11 +1,8 @@
-<h1>Birkbeck</h1>
-<section class="footer">
-  <div class="container">
-    <div class="row">
-      <div class="footer_dv">
+    <div class="container">
+      <div class="row">
+        <div class="footer_dv">
+        </div>
       </div>
     </div>
-  </div>
-</section>
   </body>
 </html>
