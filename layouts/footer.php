0f9d3b83ef4e99e8e2d6ab9de7b5a4608617cb4d
Imre Mocsari <imremocsari1@yahoo.co.uk>
2017-01-12 13:22:49 +0000

    footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="../stylesheets/footer.css">
-    <meta charset="utf-8">
-    <title>footer</title>
-  </head>
-  <body>
-    <section class="footer">
-        <div class="container">
-        	<div class="row">
-            <div class="footer_dv">
-              <p></p>
-            </div>
-          </div>
-        </div>
-    </section>
+<section class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="footer_dv">
+      </div>
+    </div>
+  </div>
+</section>
   </body>
 </html>
