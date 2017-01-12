b697b792055d356ef5e84f398ee14686f7c5fefe
Imre Mocsari <imremocsari1@yahoo.co.uk>
2017-01-12 02:08:22 +0000

    div text

--- index.php
+++ index.php
@@ -3,13 +3,23 @@ session_start();
 include("layouts/header.php"); 
 if(!empty($_GET['status'])){
   echo '<div style="padding-top:200px; padding-bottom:800px; font-weight: bold; color: white; text-align:center; font-size:30px"><p>Successfully logged out!</p><br><p style="padding-top:50px;">Redirecting to homepage!</p></div>';
-  header("Refresh: 3; url=index.php");
+  header("Refresh: 1; url=index.php");
 }
 if (isset($_SESSION['is_auth'])) {
     $name = '<div class="welcome text-center"><b>' . 'Welcome ' . $_SESSION['username'] . ' to Homepage! </b></div>';
     echo $name;
 }
 ?>
+
+
+
+
+
+
+
+
+
+
 <div class="maintext">
   <h3>The Department of <b style="color:#fcab0e;">Computer Science</b> and Information Systems at Birkbeck</h3>
     <div id="secondline">
